39a4724df6ad679e8b0b7b29d3e654529156aca9
qiangbaba <2844856384@qq.com>
2021-12-31 20:45:45 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("gggg");
         System.out.println("33333");
+        System.out.println("hot-fix test");
     }
 }
